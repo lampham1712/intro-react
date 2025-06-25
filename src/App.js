a0c6ed593773a079826a0ac9898fd5deb4681ca0
lampham1712 <pham.sylam1712@gmail.com>
2025-06-25 23:22:14 +0700

    Finish todo app

--- src/App.js
+++ src/App.js
@@ -1,31 +1,66 @@
 import React, { useState } from "react";
+import Header from "./components/Header";
 import "./App.css";
+import TaskList from "./components/TaskList";
+import AddTaskForm from "./components/AddTaskForm";
 
 function App() {
-  const [count, setCount] = useState(0);
-  let color = count > 0 ? "green" : count < 0 ? "red" : "black";
+  const [tasks, setTasks] = useState([
+    { id: "task_1", title: "Learn JS", status: 0 },
+    { id: "task_2", title: "Code a Todo List", status: 1 },
+  ]);
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-  const reset = () => setCount(0);
+  const [showIncomplete, setShowIncomplete] = useState(false);
+  const [newTask, setNewTask] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newTask) {
+      const task = {
+        id: Date.now(),
+        title: newTask,
+        status: 0,
+      };
+      setTasks([...tasks, task]);
+      setNewTask("");
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const setTaskStatus = (taskId, status) => {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === taskId) {
+          return { ...task, status: status ? 1 : 0 };
+        }
+        return task;
+      })
+    );
+  };
+
+  const removeTask = (taskId) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
 
   return (
     <div className="container">
-      <h1>Counter</h1>
-      <span style={{ color }} id="value">
-        {count}
-      </span>
-      <div className="btn-group">
-        <button onClick={decrement} className="btn btn-decrease">
-          Decrease
-        </button>
-        <button onClick={reset} className="btn btn-reset">
-          Reset
-        </button>
-        <button onClick={increment} className="btn btn-increase">
-          Increase
-        </button>
-      </div>
+      <Header title="Todo List" subTitle="Get Todo List" />
+      <TaskList
+        tasks={tasks}
+        showIncomplete={showIncomplete}
+        setTaskStatus={setTaskStatus}
+        removeTask={removeTask}
+        setShowIncomplete={setShowIncomplete}
+      />
+
+      <AddTaskForm
+        handleSubmit={handleSubmit}
+        newTask={newTask}
+        handleInputChange={handleInputChange}
+      />
     </div>
   );
 }
